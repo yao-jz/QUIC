8f70aa1bac3b77235e7fd8090238929af8c7011d
hjq <huajq19@mails.tsinghua.edu.cn>
2022-04-23 13:45:40 +0800

    debug

--- quic-starter-code/src/context/quic.hh
+++ quic-starter-code/src/context/quic.hh
@@ -67,7 +67,6 @@ class QUIC {
     uint64_t stream_count[10000];
     StreamDataReadyCallbackType streamDataReadyCallback;
     ConnectionCloseCallbackType ConnectionCloseCallback;
-    uint64_t pktnum = 0;
 };
 
 class QUICServer : public QUIC {
